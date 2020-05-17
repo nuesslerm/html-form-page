fc24eb41b9859a011db423d61063a84f367e4652
nuesslerm <markus@choco.com>
2020-05-17 16:55:39 +0200

    updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,48 @@ module.exports = {
     filename: 'bundle.js',
   },
 
-  // Default mode for Webpack is production.
-  // Depending on mode Webpack will apply different things
-  // on final bundle. For now we don't need production's JavaScript
-  // minifying and other thing so let's set mode to development
+  module: {
+    rules: [
+      {
+        /**
+         * test is a regular expression for file extension
+         * which we are going to transform. In our case it's
+         * JavaScript files.
+         */
+        test: /\.js$/,
+        /**
+         * exclude is a regular expression that tells Webpack
+         * which path should be ignored when transforming modules.
+         * That means we won't transform imported vendor libraries
+         * from npm if we import them in the future.
+         */
+        exclude: /(node_modules)/,
+        /**
+         * use is a main rule's option.
+         * Here we set the loader which is going to be applied
+         * to files that correspond to test regexp
+         * (JavaScript files in this case)
+         */
+        use: {
+          loader: 'babel-loader',
+          /**
+           * options can vary depending on loader.
+           * In this case we set default presets for Babel
+           * to consider which ES6 features it should transform and which not.
+           */
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+
+  /**
+   * Default mode for Webpack is production.
+   * Depending on mode Webpack will apply different things
+   * on final bundle. For now we don't need production's JavaScript
+   * minifying and other thing so let's set mode to development
+   */
   mode: 'development',
 };
